0dd60002f17ec94e279937276c217f4ebeb41075
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2024-04-08 22:01:27 +0900

    更新

--- select.js
+++ select.js
@@ -1,25 +1,31 @@
 (async () => {
-    const stations = await (await fetch("./data/station.json")).json();
+    const stations = (await (await fetch("./data/station.json")).json()).sort((a, b) => Number.parseInt(a.id) - Number.parseInt(b.id));
     const destSelect = document.getElementById("dest-select");
     let str = "";
     stations.forEach(sta => {
         if (sta.name == "") {
             return;
         }
-        str += `<option value="${sta.id}">[${sta.id}] ${sta.name}</option>`;
+        if (sta.name === "_") {
+            sta.name = "(無表示)";
+        }
+        str += `<option value="${sta.id}" data-id="${sta.id}" data-name="${sta.name}">${sta.name}</option>`;
     });
     destSelect.innerHTML = str;
 })();
 
 (async () => {
-    const types = await (await fetch("./data/type.json")).json();
+    const types = (await (await fetch("./data/type.json")).json()).sort((a, b) => Number.parseInt(a.id) - Number.parseInt(b.id));
     const destSelect = document.getElementById("type-select");
     let str = "";
     types.forEach(type => {
         if (type.name == "") {
             return;
         }
-        str += `<option value="${type.id}">[${type.id}] ${type.name}</option>`;
+        if (type.name === "_") {
+            type.name = "(無表示)";
+        }
+        str += `<option value="${type.id}" data-id="${type.id}" data-name="${type.name}">${type.name}</option>`;
     });
     destSelect.innerHTML = str;
 })();
