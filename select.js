f4f8997acee67bb8c0f1abe1426a4c9401ca2823
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2024-04-08 18:05:13 +0900

    方向幕を改良

--- select.js
+++ select.js
@@ -1,19 +1,25 @@
 (async () => {
-    const stations = await (await fetch("./station.json")).json();
+    const stations = await (await fetch("./data/station.json")).json();
     const destSelect = document.getElementById("dest-select");
     let str = "";
     stations.forEach(sta => {
-        str += `<option value="${sta.id}">${sta.name}</option>`;
+        if (sta.name == "") {
+            return;
+        }
+        str += `<option value="${sta.id}">[${sta.id}] ${sta.name}</option>`;
     });
     destSelect.innerHTML = str;
 })();
 
 (async () => {
-    const types = await (await fetch("./type.json")).json();
+    const types = await (await fetch("./data/type.json")).json();
     const destSelect = document.getElementById("type-select");
     let str = "";
     types.forEach(type => {
-        str += `<option value="${type.id}">${type.name}</option>`;
+        if (type.name == "") {
+            return;
+        }
+        str += `<option value="${type.id}">[${type.id}] ${type.name}</option>`;
     });
     destSelect.innerHTML = str;
 })();
